8a45bb5fd039ed0f04f04a2890539bdbc9e59b50
Avram Lubkin <aviso@rockhopper.net>
2020-05-02 12:16:12 -0700

    Bump version

--- pluginlib/__init__.py
+++ pluginlib/__init__.py
@@ -10,7 +10,7 @@
 A framework for creating and importing plugins
 """
 
-__version__ = '0.6.2'
+__version__ = '0.7.0'
 
 __all__ = ['abstractmethod', 'abstractproperty', 'abstractstaticmethod', 'abstractclassmethod',
            'abstractattribute', 'BlacklistEntry', 'EntryPointWarning', 'Parent', 'Plugin',
