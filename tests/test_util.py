078819c7c5fa4bc5110dd13e9d9e3ab033ad23c6
Avram Lubkin <aviso@rockhopper.net>
2023-08-25 18:36:09 -0400

    Add copyright check

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,4 @@
-# Copyright 2014 - 2018 Avram Lubkin, All Rights Reserved
+# Copyright 2014 - 2022 Avram Lubkin, All Rights Reserved
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
