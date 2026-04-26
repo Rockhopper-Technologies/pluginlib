eda1a4811cc59a439927870e0f62fd4d989334fd
Avram Lubkin <aviso@rockhopper.net>
2026-04-26 08:29:41 -0400

    Update Classifiers

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2018 - 2025 Avram Lubkin, All Rights Reserved
+# Copyright 2018 - 2026 Avram Lubkin, All Rights Reserved
 
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -46,6 +46,8 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: 3.15',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
